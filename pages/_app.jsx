6803f32bb175cc7a2011140a3e94c85f3962b13c
raslaner115 <rassdhhl@gmail.com>
2026-05-04 12:11:07 +0300

    .

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,32 @@
 import Head from 'next/head';
+import { useEffect } from 'react';
 import { AuthProvider }     from '../contexts/AuthContext';
 import { LanguageProvider } from '../contexts/LanguageContext';
 import Layout from '../components/layout/Layout';
 import SiteFooter from '../components/layout/SiteFooter';
 import { Toaster } from 'react-hot-toast';
+import { startForegroundPushNotifications } from '../lib/notifications';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   // Allow pages to opt out of the shared Layout (e.g. auth pages)
   const getLayout = Component.getLayout ?? ((page) => <Layout>{page}</Layout>);
+
+  useEffect(() => {
+    let unsubscribe = null;
+
+    startForegroundPushNotifications()
+      .then((nextUnsubscribe) => {
+        unsubscribe = nextUnsubscribe;
+      })
+      .catch(() => {});
+
+    return () => {
+      if (typeof unsubscribe === 'function') {
+        unsubscribe();
+      }
+    };
+  }, []);
 
   return (
     <LanguageProvider>
